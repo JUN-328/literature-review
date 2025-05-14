41f0a01068ad414ab11e2b7ce72b656e199c8b87
JUN-328 <1191376880@qq.com>
2025-05-14 19:03:40 +0100

    Initial SurVis upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,108 @@
+﻿const generatedBibEntries = {
+    "berti2024pm": {
+        "author": "Berti, Alessandro and Kourani, Humam and van der Aalst, Wil MP",
+        "booktitle": "International Conference on Process Mining",
+        "doi": "10.1007/978-3-031-82225-4_45",
+        "organization": "Springer",
+        "pages": "610--623",
+        "publisher": "Springer",
+        "title": "{PM}-{LLM}-Benchmark: Evaluating large language models on process mining tasks",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "chen2024sheetagent": {
+        "author": "Chen, Yibin and Yuan, Yifu and Zhang, Zeyu and Zheng, Yan and Liu, Jinyi and Ni, Fei and Hao, Jianye",
+        "booktitle": "ICML 2024 Workshop on LLMs and Cognition",
+        "doi": "10.1145/3696410.3714962",
+        "pages": "158 - 177",
+        "publisher": "ACM",
+        "title": "{SheetAgent}: A generalist agent for spreadsheet reasoning and manipulation via large language models",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "kumar2024large": {
+        "author": "Kumar, Pranjal",
+        "doi": "10.1007/s10462-024-10888-y",
+        "journal": "Artificial Intelligence Review",
+        "number": "10",
+        "pages": "260",
+        "publisher": "Springer",
+        "title": "{Large Language Models (LLMs)}: survey, technical frameworks, and future challenges",
+        "type": "article",
+        "volume": "57",
+        "year": "2024"
+    },
+    "lu2025large": {
+        "author": "Lu, Weizheng and Zhang, Jing and Fan, Ju and Fu, Zihao and Chen, Yueguo and Du, Xiaoyong",
+        "doi": "10.1007/s11704-024-40763-6",
+        "journal": "Frontiers of Computer Science",
+        "number": "2",
+        "pages": "192350",
+        "publisher": "Springer",
+        "title": "{Large Language Model} for Table Processing: A Survey",
+        "type": "article",
+        "volume": "19",
+        "year": "2025"
+    },
+    "padmanabhan2010interactive": {
+        "author": "Padmanabhan, Raghav Krishna and Jandhyala, Ramana Chakradhar and Krishnamoorthy, Mukkai and Nagy, George and Seth, Sharad and Silversmith, William",
+        "booktitle": "Graphics Recognition. Achievements, Challenges, and Evolution: 8th International Workshop, GREC 2009",
+        "doi": "10.1007/978-3-642-13728-0_3",
+        "pages": "25--36",
+        "publisher": "Springer",
+        "title": "Interactive conversion of web tables",
+        "type": "inproceedings",
+        "year": "2010"
+    },
+    "redis2024skill": {
+        "author": "Redis, Andrei Cosmin and Sani, Mohammadreza Fani and Zarrin, Bahram and Burattin, Andrea",
+        "booktitle": "International Conference on Process Mining",
+        "doi": "10.1007/978-3-031-82225-4_48",
+        "pages": "650--662",
+        "publisher": "Springer",
+        "title": "Skill Learning Using Process Mining for Large Language Model Plan Generation",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "singh2023format5": {
+        "author": "Singh, Mukul and Cambronero, Jos\u00e9 and Gulwani, Sumit and Le, Vu and Negreanu, Carina and Nouri, Elnaz and Raza, Mohammad and Verbruggen, Gust",
+        "doi": "10.14778/3632093.3632111",
+        "journal": "Proceedings of the VLDB Endowment",
+        "number": "3",
+        "pages": "497--510",
+        "publisher": "VLDB Endowment",
+        "title": "Format5: Abstention and examples for conditional table formatting with natural language",
+        "type": "article",
+        "volume": "17",
+        "year": "2023"
+    },
+    "wulff2025natural": {
+        "author": "Wulff, Peter and Kubsch, Marcus and Krist, Christina",
+        "booktitle": "Applying Machine Learning in Science Education Research: When, How, and Why?",
+        "pages": "117--142",
+        "publisher": "Springer Nature Switzerland Cham",
+        "title": "Natural Language Processing and Large Language Models",
+        "type": "incollection",
+        "year": "2025"
+    },
+    "ye2023large": {
+        "author": "Ye, Yunhu and Hui, Binyuan and Yang, Min and Li, Binhua and Huang, Fei and Li, Yongbin",
+        "booktitle": "Proceedings of the 46th International ACM SIGIR Conference on Research and Development in Information Retrieval",
+        "doi": "10.1145/3539618.3591708",
+        "pages": "174--184",
+        "publisher": "ACM",
+        "title": "{Large Language Models} are Versatile Decomposers: Decomposing Evidence and Questions for Table-based Reasoning",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "zhang2014towards": {
+        "author": "Zhang, Ziqi",
+        "booktitle": "The Semantic Web--ISWC 2014",
+        "doi": "10.1007/978-3-319-11964-9_31",
+        "pages": "487--502",
+        "publisher": "Springer",
+        "title": "Towards Efficient and Effective Semantic Table Interpretation",
+        "type": "inproceedings",
+        "year": "2014"
+    }
+};
